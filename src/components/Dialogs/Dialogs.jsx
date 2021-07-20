02dfca8766235f97328d49eda46ba77b8a0f0a1c
valnur <valnurray@gmail.com>
2021-07-20 18:51:10 +0300

    five

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,34 @@ import classes from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog}>
+                    Vasya
+                </div>
+
+                <div className={classes.dialog}>
+                    Ivan
+                </div>
+
+                <div className={classes.dialog}>
+                    Makar
+                </div>
+
+                <div className={classes.dialog}>
+                    Sveta
+                </div>
+
+                <div className={classes.dialog}>
+                    Dasha
+                </div>
+
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}> Hi</div>
+                <div className={classes.message}> Buy</div>
+                <div className={classes.message}> Wai</div>
+            </div>
         </div>
     );
 }
