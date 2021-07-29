b4f039b822c468c6f1e4ffc17af4ecea7f4a8746
valnur <valnurray@gmail.com>
2021-07-29 18:29:23 +0300

    clean Side Effect from /users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,18 +5,23 @@ import userPhoto from "../../assets/images/user.jpeg"
 
 let Users = (props) => {
 
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items);
-        });
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items);
+            });
+        }
     }
 
+
     return <div>
+        <button onClick={getUsers}> Get users</button>
         {
             props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto}
+                                 className={styles.userPhoto}/>
                         </div>
                         <div>
                                        {u.followed
