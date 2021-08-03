b67b807849711bc46773abfd07a4aeb74726d276
valnur <valnurray@gmail.com>
2021-08-03 18:50:53 +0300

    auth

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -37,14 +37,11 @@ let Users = (props) => {
 
                         </div>
                         <div>
-                                       {u.followed
-                                           ? <button onClick={() => {
-                                               props.unfollow(u.id)
-                                           }}> Unfollow </button>
-                                           : <button onClick={() => {
-                                               props.follow(u.id)
-                                           }}> Follow </button>
-                                       }
+                             {
+                                 u.followed
+                                 ? <button onClick={() => { props.unfollow(u.id)}}> Unfollow </button>
+                                 : <button onClick={() => {props.follow(u.id)}}> Follow </button>
+                             }
                         </div>
                     </span>
                 <span>
